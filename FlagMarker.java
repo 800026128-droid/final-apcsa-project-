4bc9a870f1e29355ed7f398c06c8fcc20ca0270b
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-04 19:20:31 -0800

    Simplify FlagMarker constructor and act method
    Refactor FlagMarker class by simplifying constructor and act method.

--- FlagMarker.java
+++ FlagMarker.java
@@ -1,23 +1,17 @@
+/**
+ * FlagMarker - Marks special tiles on the game map, e.g., finish or checkpoint.
+ */
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class FlagMarker here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class FlagMarker extends Actor
 {
     /**
-     * Act - do whatever the FlagMarker wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor - Sets up the flag marker.
      */
-    public FlagMarker()
-    {
-        //Constructor
-    }
-    public void act()
-    {
-        // Add your action code here.
-    }
+    public FlagMarker() {}
+
+    /**
+     * Act - Placeholder for future flag interactions.
+     */
+    public void act() {}
 }
