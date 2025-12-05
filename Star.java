299de28006654f9a3b8e6862c56ee69ab8716591
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-04 19:36:10 -0800

    Refactor Star class documentation
    Updated class documentation for clarity and conciseness.

--- Star.java
+++ Star.java
@@ -1,19 +1,15 @@
+/**
+ * Star - Beneficial item that grants the player an extra life when revealed.
+ */
 import greenfoot.*;
 
 /**
- * Star - A beneficial item in PandaQuest.
- *
- * When revealed, a star gives the player an extra life,
- * acting as the opposite of Bamboo.
- *
- * @author PandaQuest Team
- * @version 1.0
+ * Star actor - static beneficial tile indicator.
  */
 public class Star extends Actor
 {
     /**
-     * Constructor for Star.
-     * The image for this actor should be set in Greenfoot (star.png or similar).
+     * Constructor - Initializes star actor.
      */
     public Star()
     {
@@ -21,7 +17,7 @@ public class Star extends Actor
     }
 
     /**
-     * Act - Star is static and doesn't need to do anything per act cycle.
+     * Act - Star has no per-frame behavior.
      */
     public void act()
     {
