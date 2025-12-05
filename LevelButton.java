b0487eca41c3651cfb8d6c21aabb5d7183f11a75
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-04 19:24:59 -0800

    Simplify image handling in LevelButton class
    Refactor act method to simplify image handling.

--- LevelButton.java
+++ LevelButton.java
@@ -1,39 +1,41 @@
+/**
+ * LevelButton - Button to start the main game; changes style on hover.
+ */
 import greenfoot.*;
 
 public class LevelButton extends Actor
 {
     private static final GreenfootImage NORMAL_IMAGE = new GreenfootImage("levelButton_normal.png");
     private static final GreenfootImage HOVER_IMAGE = new GreenfootImage("levelButton_faded.png");
-    
+
+    /**
+     * Constructor - Sets default image for level button.
+     */
     public LevelButton()
     {
-        // Set the initial image
         setImage(NORMAL_IMAGE);
     }
 
+    /**
+     * Act - Starts game world on click, and updates button image on hover.
+     */
     public void act()
     {
-        // 1. Handle the click action (same as before)
         if (Greenfoot.mouseClicked(this))
         {
             Greenfoot.setWorld(new PandaWorld());
-            return; 
+            return;
         }
 
-        // 2. Handle the hover action: Switch images
         if (Greenfoot.mouseMoved(this))
         {
-            // If the mouse is over the button, set the pre-faded image
-            if (getImage() != HOVER_IMAGE) {
+            if (getImage() != HOVER_IMAGE)
                 setImage(HOVER_IMAGE);
-            }
         }
         else
         {
-            // If the mouse is NOT over the button, set the normal image
-            if (getImage() != NORMAL_IMAGE) {
+            if (getImage() != NORMAL_IMAGE)
                 setImage(NORMAL_IMAGE);
-            }
         }
-    }    
+    }
 }
