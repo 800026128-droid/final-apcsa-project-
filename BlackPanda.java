41fac39cd86c08ec24f3de3c0986e3b4ed5a76f3
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-04 19:16:54 -0800

    Refactor BlackPanda class with comments and formatting
    Added comments and improved code formatting.

--- BlackPanda.java
+++ BlackPanda.java
@@ -1,22 +1,29 @@
+/**
+ * BlackPanda - Selectable panda skin that sets the player's color when clicked.
+ */
 import greenfoot.*;
 
 public class BlackPanda extends Actor
-{ 
+{
     private static final GreenfootImage NORMAL_IMAGE = new GreenfootImage("blackPanda.PNG");
 
+    /**
+     * Constructor - Sets black panda image.
+     */
     public BlackPanda()
     {
-        // Set the initial image
         setImage(NORMAL_IMAGE);
     }
 
+    /**
+     * Act - Changes the panda color on click.
+     */
     public void act()
     {
-        // 1. Handle the click action (same as before)
         if (Greenfoot.mouseClicked(this))
         {
             Levels.color = 7;
-            return; 
+            return;
         }
-    }    
+    }
 }
