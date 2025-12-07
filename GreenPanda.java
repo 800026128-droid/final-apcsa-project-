c663880af4dd88f6806b01c8c0d6bbb54f90bbf3
Jocelyn Mathew <49656538+josiemath@users.noreply.github.com>
2025-12-06 21:36:19 -0800

    Enhance GreenPanda with click sound and comments
    Updated GreenPanda class to play sound on click and improved documentation.

--- GreenPanda.java
+++ GreenPanda.java
@@ -1,12 +1,13 @@
 /**
- * GreenPanda - Selectable panda skin that sets the player's color when clicked.
+ * GreenPanda - Selectable panda skin; sets avatar color to green when clicked.
  */
 import greenfoot.*;
 
 public class GreenPanda extends Actor
-{
+{ 
     private static final GreenfootImage NORMAL_IMAGE = new GreenfootImage("greenPanda.PNG");
-
+    private GreenfootSound clickSound = new GreenfootSound("SelectSound.mp3");
+    
     /**
      * Constructor - Sets green panda image.
      */
@@ -23,7 +24,8 @@ public class GreenPanda extends Actor
         if (Greenfoot.mouseClicked(this))
         {
             Levels.color = 3;
-            return;
+            clickSound.play();
+            return; 
         }
-    }
+    }    
 }
